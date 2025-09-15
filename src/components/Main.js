ffb691c3eb7d46180c3352e2ae421af9caf81b2c
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-09-15 22:01:58 +0700

    stacking grids

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,21 @@ function Main() {
     <main className='App-main'>
       Main: The restaurant features a locally-sourced menu with a focus on
       fresh, locally-grown ingredients
+      <br />
+      <div className='grid-adjustable-columns'>
+        <div className='grid-item'>1</div>
+        <div className='grid-item'>2</div>
+        <div className='grid-item'>3</div>
+        <div className='grid-item'>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>10</div>
+        <div>11</div>
+        <div>12</div>
+      </div>
     </main>
   );
 }
