b9a1788fa6a8fb47aa7ba20a204bf0a6cd6e6585
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-09-19 16:31:00 +0700

    change on nav and about contents

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,16 @@ function About() {
     <div className='nav-elements'>
       <h2>About Us</h2>
       <p>
-        Little Lemon is a family owned Mediterranean restaurant, focused on
-        traditional recipes served with a modern twist.
+        Little Lemon Chicago is a family owned Mediterranean restaurant located
+        in the heart of the city. The restaurant is run by brothers Mario and
+        Adrian, who have always had a passion for cooking and serving delicious
+        food. Growing up in a Mediterranean household, the brothers were exposed
+        to traditional recipes from an early age, and they decided to bring
+        those recipes to the masses with a modern twist. At Little Lemon, you
+        can expect to find a menu full of classic dishes with a creative twist
+        that makes them stand out from the rest. Whether you're looking for a
+        quick lunch or a leisurely dinner, Little Lemon Chicago is the perfect
+        place to indulge in a delicious meal in a cozy and welcoming atmosphere.
       </p>
     </div>
   );
--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,17 +1,13 @@
 import { useState } from 'react';
 
 export const BookingPage = () => {
-  return <div>Reservation Page</div>;
-};
-
-export const BookingForm = () => {
   const [availableTimes, setAvailableTimes] = useState([]);
   const [guests, setGuests] = useState(2);
   const [occasion, setOccasion] = useState('Birthday');
 
   return (
     <div>
-      <h1>BookingForm</h1>
+      <h1>Booking Form</h1>
       <div>
         <label>Date:</label>
         <input type='date' />
@@ -30,9 +26,10 @@ export const BookingForm = () => {
         <select value={occasion} onChange={(e) => setOccasion(e.target.value)}>
           <option value='Birthday'>Birthday</option>
           <option value='Anniversary'>Anniversary</option>
+          <option value='Wedding'>Wedding</option>
+          <option value='Party'>Party</option>
         </select>
       </div>
-      a
     </div>
   );
 };
