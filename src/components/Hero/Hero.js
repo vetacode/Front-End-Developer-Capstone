22c82ac9da767a97193e35831774d589f4f5a8d7
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-09-21 10:35:28 +0700

    styling Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,17 +4,20 @@ import hero from '../../assets/images/hero-image.svg';
 
 const Hero = () => {
   return (
-    <section name='home' className='hero'>
+    <section name='home' className='hero aria-labelledby="hero-title'>
       <div className='display-container'>
-        <h1 className='display-title'>Little Lemon</h1>
+        <h1 id='hero-title' className='display-title'>
+          Little Lemon
+        </h1>
         <h3 className='display-subtitle'>Chicago</h3>
         <h4 className='display-text'>
           We are a family owned Mediterranean restaurant, focused on traditional
           recipes served with a modern twist.
         </h4>
-        <LinkR to='/bookingPage'>
+        <LinkR to='/bookingPage' aria-label='reserve a table'>
           <button
             className='btn'
+            aria-label='reserve a table'
             style={{
               width: '300px',
               height: '50px',
